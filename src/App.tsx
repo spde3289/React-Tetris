c0bc77e44e3dc421add1d199f337952189495e48
spde3289 <spde3289@naver.com>
2023-06-11 00:31:26 +0900

    feat: package install

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+
+function App() {
+  return (
+    <Container>
+      ASDASD
+    </Container>
+  )
+}
+
+const Container = styled.div`
+  
+`
+
+export default App
